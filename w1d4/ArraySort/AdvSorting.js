8fbb6475b33133ec665e4e522c3d966b8b986353
Michael Stranges <strangesm@gmail.com>
2018-03-15 18:35:05 +0000

    added descending feature

--- w1d4/ArraySort/AdvSorting.js
+++ w1d4/ArraySort/AdvSorting.js
@@ -8,6 +8,7 @@ var students = [
 students.sort(function(a, b){
   var name1 = a.name;
   var name2 = b.name;
+
   if(name1 < name2){
     return -1;
   }
@@ -15,13 +16,12 @@ students.sort(function(a, b){
     return 1;
   }
   if(name1 === name2){
-    students.sort(function(a,b){
       var age1 = a.age;
       var age2 = b.age;
+
         if (age1 < age2){
-          return -1;
+          return 1;
         }
-    })
   }
 })
 
