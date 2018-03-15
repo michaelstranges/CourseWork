57455ae26cc6ca859c6ff91d05cf423836ec918a
Michael Stranges <strangesm@gmail.com>
2018-03-15 17:48:34 +0000

    introduced sorting features name and id

---
+++ w1d4/ArraySort/AdvSorting.js
@@ -0,0 +1,28 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b){
+  var name1 = a.name;
+  var name2 = b.name;
+  if(name1 < name2){
+    return -1;
+  }
+  if(name1 > name2){
+    return 1;
+  }
+  if(name1 === name2){
+    students.sort(function(a,b){
+      var age1 = a.age;
+      var age2 = b.age;
+        if (age1 < age2){
+          return -1;
+        }
+    })
+  }
+})
+
+console.log(students);
