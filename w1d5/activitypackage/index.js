7c46925f8136c3840885a6de158bcb1874b173f1
Michael Stranges <strangesm@gmail.com>
2018-03-16 17:02:01 +0000

    create w1d5

---
+++ w1d5/activitypackage/index.js
@@ -0,0 +1,9 @@
+var chalk = require("chalk");
+
+var message = "Hello " + chalk.yellow("World");
+var message1 = "Hello " + chalk.red("World");
+var message2 = "Hello " + chalk.blue("World");
+
+console.log(message);
+console.log(message1);
+console.log(message2);
